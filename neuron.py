4518e8d3bbc59408fc761227d49b0b4534a6457c
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2022-06-18 16:04:56 -0700

    Write Basic Neuron Introduction

--- neuron.py
+++ neuron.py
@@ -1,1 +1,14 @@
-# Basic neuron code
+# Basic neuron code - somewhere in our fully connected neural network
+# Every neuron has a unique connection to each previous neuron
+
+# The outputs of the previous neurons are the inputs of this one
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
+
+output = (inputs[0] * weights[0] 
+  + inputs[1] * weights[1] 
+  + inputs[2] * weights[2] 
+  + bias)
+
+print(output)
